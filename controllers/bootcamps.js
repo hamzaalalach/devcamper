19ae8a706ba4f8046362c05e4604533120146753
Hamza Alalach <hamzaalalach@gmail.com>
2021-01-26 02:37:47 +0100

    Add controller/routes logic

---
+++ controllers/bootcamps.js
@@ -0,0 +1,9 @@
+exports.getBootcamps = (req, res, next) => {};
+
+exports.getBootcamp = (req, res, next) => {};
+
+exports.createBootcamp = (req, res, next) => {};
+
+exports.updateBootcamp = (req, res, next) => {};
+
+exports.deleteBootcamp = (req, res, next) => {};
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`));
