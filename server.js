4cb9d430f5c8307ad3004700c9369ed64a646f02
Hamza Alalach <hamzaalalach@gmail.com>
2021-02-09 17:52:24 +0100

    Handle unhandledRejetions

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
-
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,4 +15,13 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1));
+});
