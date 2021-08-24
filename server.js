6f35d0d3027d3ae1fc83b0f67ccf595d40a19c7a
Hamza Alalach <hamzaalalach@gmail.com>
2021-08-24 02:14:27 +0200

    Add login and register

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
 const colors = require('colors');
+const cookieParser = require('cookie-parser');
 const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 
 dotenv.config({ path: './config/config.env' });
 
 const connectDB = require('./config/db');
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -24,8 +24,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(fileupload());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
+app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
+app.use('/api/v1/courses', require('./routes/courses'));
+app.use('/api/v1/auth', require('./routes/auth'));
 app.use(errorHandler);
 
 const server = app.listen(
