8aca09a8c6fc40d749905983952b60a033bc188f
Hamza Alalach <hamzaalalach@gmail.com>
2021-11-29 22:55:13 +0100

    Add cors hpp ratelimiter

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const colors = require('colors');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
@@ -17,6 +20,10 @@ const connectDB = require('./config/db');
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,6 +36,9 @@ app.use(fileupload());
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
 app.use('/api/v1/courses', require('./routes/courses'));
