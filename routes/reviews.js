e4644399f1bf5014f5f636fe71b45e31c149a4fc
Hamza Alalach <hamzaalalach@gmail.com>
2021-11-29 01:06:26 +0100

    Implement get single review

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const advancedResuts = require('../middleware/advancedResults');
 const router = express.Router({ mergeParams: true });
 const { protect, authorize } = require('../middleware/auth');
-const { getReviews } = require('../controllers/reviews');
+const { getReviews, getReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 
 router
@@ -13,6 +13,10 @@ router
       select: 'name description'
     }),
     getReviews
-  )
+  );
+
+router
+  .route('/:id')
+  .get(getReview);
 
 module.exports = router;
