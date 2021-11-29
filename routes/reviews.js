01b0e67f67e318d6a0ef51ea38d37b6a0d7019a8
Hamza Alalach <hamzaalalach@gmail.com>
2021-11-29 22:11:11 +0100

    Implement update delete review

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const advancedResuts = require('../middleware/advancedResults');
 const router = express.Router({ mergeParams: true });
 const { protect, authorize } = require('../middleware/auth');
-const { getReviews, getReview, addReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview, updateReview, deleteReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 
 router
@@ -18,6 +18,8 @@ router
 
 router
   .route('/:id')
-  .get(getReview);
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
